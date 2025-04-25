16aaa18a60db46b04c9b2b4ea013e1d2da118a07
RickyBv1 <rickycortes668668@gmail.com>
2025-04-24 21:34:47 -0400

    feat: add Todo component and integrate todomvc-app-css for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import './App.css'
+import { useState } from "react";
+import { Todos } from "./components/Todos";
 
-function App() {
+const mockTodos = [
+  {
+    id: "1",
+    title: "to do 1",
+    completed: false,
+  },
+  {
+    id: "2",
+    title: "to do 2",
+    completed: false,
+  },
+  {
+    id: "3",
+    title: "to do 3",
+    completed: false,
+  },
+];
+
+const App = () => {
+  const [todos] = useState(mockTodos);
 
   return (
     <>
+      <Todos todos={todos} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
