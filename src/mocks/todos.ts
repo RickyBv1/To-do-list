23c5288c8f487904ae1a05b19e698ad9c1f905ee
RickyBv1 <rickycortes668668@gmail.com>
2025-05-02 18:54:16 -0400

    feat: add mock data for completed, active, and all todos

---
+++ src/mocks/todos.ts
@@ -0,0 +1,53 @@
+export const mockAllCompletedTodos = [
+  {
+    completed: true,
+    id: "7b6d5f38-e510-4409-aeb0-1f6f6422384e",
+    title: "Make a todo app",
+  },
+  {
+    completed: true,
+    id: "efad0afc-7d2e-4020-8ef4-14fd0b832de8",
+    title: "Ride the bike",
+  },
+  {
+    completed: true,
+    id: "6a3d0d0f-d2d6-4d2a-9b08-5a5d8a5e0c1d",
+    title: "Go to the movies",
+  },
+];
+
+export const mockAllActiveTodos = [
+  {
+    completed: false,
+    id: "c19f8c9b-ae32-4c8a-9bed-d141b09f5477",
+    title: "Make some exercise",
+  },
+  {
+    completed: false,
+    id: "efad0afc-7d2e-4020-8ef4-14fd0b832de8",
+    title: "Make some lunch",
+  },
+  {
+    completed: false,
+    id: "6a3d0d0f-d2d6-4d2a-9b08-5a5d8a5e0c1d",
+    title: "Upload the project to GitHub",
+  },
+];
+
+export const mockTodos = [
+  {
+    completed: false,
+    id: "c19f8c9b-ae32-4c8a-9bed-d141b09f5477",
+    title: "Take the dog to the vet",
+  },
+  {
+    completed: true,
+    id: "efad0afc-7d2e-4020-8ef4-14fd0b832de8",
+    title: "Buy bread",
+  },
+  {
+    completed: false,
+    id: "6a3d0d0f-d2d6-4d2a-9b08-5a5d8a5e0c1d",
+    title: "Read a book",
+  },
+];
