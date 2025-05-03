612aeb5b3307f6f9b8778dd2e731f4e36d7ab2b5
RickyBv1 <rickycortes668668@gmail.com>
2025-05-02 21:36:58 -0400

    feat: implement fetchTodos and updateTodos functions for API interaction

---
+++ src/services/todos.ts
@@ -0,0 +1,39 @@
+import { type TodoList } from "../types.d";
+
+const API_URL = "https://api.jsonbin.io/v3/b/63ff3a52ebd26539d087639c";
+
+interface Todo {
+  id: string;
+  title: string;
+  completed: boolean;
+  order: number;
+}
+
+export const fetchTodos = async (): Promise<Todo[]> => {
+  const res = await fetch(API_URL);
+  if (!res.ok) {
+    console.error("Failed to fetch todos");
+    return [];
+  }
+
+  const { record: todos } = (await res.json()) as { record: Todo[] };
+  return todos;
+};
+
+export const updateTodos = async ({
+  todos,
+}: {
+  todos: TodoList;
+}): Promise<boolean> => {
+  const res = await fetch(API_URL, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Master-Key": import.meta.env.VITE_API_BIN_KEY,
+    },
+
+    body: JSON.stringify(todos),
+  });
+
+  return res.ok;
+};
